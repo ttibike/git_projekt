7ffd9d0e7f94c13da913ea26d3542f258c4ff4b6
ttibike <75677197+ttibike@users.noreply.github.com>
2021-01-05 13:24:06 +0100

    Add files via upload

---
+++ munka.php
@@ -0,0 +1,23 @@
+<?php
+
+// Matematikai műveletek
+//===============================================
+
+// Összeadás
+//------------------
+
+$a = 105;
+$b = 17;
+
+
+$c  = 12;
+print $a+$b+$c;
+//A konfliktus megoldódott
+// Kivonás
+//------------------
+$x = 35;
+$y = 19;
+print "<br /><br />" . $x - $y;
+//Ez a kivonás ág
+//konfliktus lesz
+?>
